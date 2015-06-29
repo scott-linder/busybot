059e0bed3a8e39a0100d8902a951f9ece1abb116
scott-linder <scott.b.linder@wmich.edu>
2015-06-29 16:18:22 -0400

    Fix bug where quoted whitespace was clobbered

--- lib/cmd.sh
+++ lib/cmd.sh
@@ -4,7 +4,7 @@ nick=$1; shift
 chan=$1; shift
 
 if [ -n "$opts" ]; then
-    O=`getopt -s sh $opts $@ 2>&1`
+    O=`getopt -s sh "$opts" "$@" 2>&1`
     if [ $? != 0 ]; then
         privmsg $chan "$O"
         exit 1
