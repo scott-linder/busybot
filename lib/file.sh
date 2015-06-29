cd67952928da56d0dd471f011a30f28367021408
scott-linder <scott.b.linder@wmich.edu>
2015-06-29 10:50:22 -0400

    Add per-chan files, updating $echo and adding $cat

--- lib/file.sh
+++ lib/file.sh
@@ -1,3 +1,10 @@
 file_sanitize_path() {
     echo "$1" | tr -d ' ./'
 }
+
+file_chan() {
+    local chan=`file_sanitize_path "$1"`
+    local file=`file_sanitize_path "$2"`
+    mkdir -p var/chan/$chan
+    echo var/chan/$chan/$file
+}
